aab8c57d55f697e2bc01a90fdaec60bfe8475a84
scotty-kotty-myer <ckottmyer@salesforce.com>
2019-04-08 19:08:20 -0400

    gallery

--- assets/js/discover.js
+++ assets/js/discover.js
@@ -3,12 +3,13 @@
 function createGame(json) {
     
     var contentPage = $("#discover-tool");
+    var baseUrl = '/media/images/game-icons/' + json.id + '/'
     
-    var image = "/media/images/game-icons/" + json["main-image"]
-    var product_url = "/pages/product.html?game=" + json.id
+    var image = baseUrl + json["main-image"];
+    var product_url = "/pages/product.html?game=" + json.id;
     
     var gameFrame = document.createElement("div");
-    var gameTitle = document.createElement("h1")
+    var gameTitle = document.createElement("h1");
     var gameLink = document.createElement("a");
     var gameImage = document.createElement("img");
     
@@ -19,12 +20,23 @@ function createGame(json) {
     $(gameImage).addClass("col-s-12 col-m-12 col-l-12")
         .attr("src",image).appendTo(gameLink);
     $(gameFrame).appendTo(contentPage);
+    
+    json['gallery'].forEach(function(e){
+        e.href = baseUrl + e.href;
+    })
+    
+    $(gameLink).click( function( e ) {
+        e.preventDefault();
+        $.swipebox(json['gallery']);
+    });
 
+    
+    
     return 0;
 }
 
 function createProductSearch(json){
-    for (let key in json){
+    for (var key in json){
         if (json.hasOwnProperty(key)){
             var game = json[key];
             createGame(game);
@@ -36,7 +48,9 @@ function getGameData() {
     var xhttp = new XMLHttpRequest(); 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            alert('hello world');
            var gameData = JSON.parse(xhttp.responseText);
+            alert(gameData);
            createProductSearch(gameData);
         } else if (this.readyState == 4 && this.status != 200) {
             window.replace('/pages/error/404.html');
--- assets/js/product.js
+++ assets/js/product.js
@@ -1,21 +1,18 @@
 'use strict'
 
-function getGamesData() {
-    var xhttp = new XMLHttpRequest(); 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            return JSON.parse(xhttp.responseText);
-        } else {
-            return {
-                "error" : 404;
-            }
-        }
-    };     
-    xhttp.open("GET", "/data/games.json", true);
-    xhttp.send();
+function constructProductPage(json) {
+    var current_game = getUrlParameter('game');
+    var contentSection = $("#content");
+    
+    var gameTitleText = json[current_game].title;
+    var gameTitle = document.createElement("h1");
+    
+    $(gameTitle).text(gameTitleText).appendTo(contentSection);
+    
+    return 0;
 }
 
-var getUrlParameter = function getUrlParameter(sParam) {
+function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
@@ -28,55 +25,25 @@ var getUrlParameter = function getUrlParameter(sParam) {
             return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
         }
     }
-};
+}
 
-<div class="col-s-12 col-m-6 col-l-4">
-    <h1>Gloomhaven</h1>
-    <a href="/pages/product.html?game=gloomhaven">
-     <img class="col-s-12 col-m-12 col-l-12" src="/media/images/game-icons/gloomhaven.jpg" aria-controls=""alt="boardemic logo">
-    </a>
-</div> 
+function getGameData() {
+    var xhttp = new XMLHttpRequest(); 
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+           var gameData = JSON.parse(xhttp.responseText);
+           constructProductPage(gameData);
+        } else if (this.readyState == 4 && this.status != 200) {
+            window.replace('/pages/error/404.html');
+        }
+    };     
+    xhttp.open("GET", "/data/games.json", true);
+    xhttp.send();
+}
 
 $(document).ready(function(){
-    
     var err
     var errUrl
-    var games = getGamesData();
     
-    if (games.hasOwnProperty('error')){
-        var errUrl = '/pages/error/' + games['error'] + 'html'; 
-    } else {
-        var current_game = getUrlParameter('game');
-        if (! games.hasOwnProperty(current_game)) {
-            var errUrl = '/pages/error/404.html'
-        }
-    }
-    
-    if (err == undefined) {
-        window.replace(errUrl);
-    } 
-    
-    var gameTitle = games[current_game].title;
-    
-    var gameBlock = document.createElement('div')
-        .id(current_game)
-        .addClass("col-s-12")
-        .addClass("col-m-6")
-        .addClass("col-l-12")
-        .appendTo("#content")
-    
-    var gameTitle = document.createElement('h1')
-        .textContent(current_game)
-        .appendTo(current_game)
-    
-    var gameLink = document.createElement('a')
-        .href = "/pages/product.html?game=" + current_game;
-        .appendTo(current_game)
-    
-    var gameImage = document.createElement('img')
-        .addClass("col-s-12")
-        .addClass("col-m-12")
-        .addClass("col-l-12")
-        .alt(current_game)
-        .src("/media/images/game-icons/" + current_game + '.jpg')
-});
+    getGameData();
+})
