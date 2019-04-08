2e2cc04f7288a868e588a51123ff12f10ac825a5
scotty-kotty-myer <ckottmyer@salesforce.com>
2019-04-08 13:11:41 -0400

    updating

--- assets/js/discover.js
+++ assets/js/discover.js
@@ -1,14 +1,45 @@
 'use strict'
 
-function getGamesData() {
+function createGame(json) {
+    
+    var contentPage = $("#discover-tool");
+    
+    var image = "/media/images/game-icons/" + json["main-image"]
+    var product_url = "/pages/product.html?game=" + json.id
+    
+    var gameFrame = document.createElement("div");
+    var gameTitle = document.createElement("h1")
+    var gameLink = document.createElement("a");
+    var gameImage = document.createElement("img");
+    
+    $(gameFrame).addClass("col-s-12 col-m-6 col-l-4");
+    $(gameTitle).text(json.title).appendTo(gameFrame);
+    $(gameLink).attr("href",product_url)
+        .appendTo(gameFrame);
+    $(gameImage).addClass("col-s-12 col-m-12 col-l-12")
+        .attr("src",image).appendTo(gameLink);
+    $(gameFrame).appendTo(contentPage);
+
+    return 0;
+}
+
+function createProductSearch(json){
+    for (let key in json){
+        if (json.hasOwnProperty(key)){
+            var game = json[key];
+            createGame(game);
+        }
+    }
+}
+
+function getGameData() {
     var xhttp = new XMLHttpRequest(); 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            return JSON.parse(xhttp.responseText);
-        } else {
-            return {
-                "error" : 404;
-            }
+           var gameData = JSON.parse(xhttp.responseText);
+           createProductSearch(gameData);
+        } else if (this.readyState == 4 && this.status != 200) {
+            window.replace('/pages/error/404.html');
         }
     };     
     xhttp.open("GET", "/data/games.json", true);
@@ -16,5 +47,5 @@ function getGamesData() {
 }
 
 $(document).ready(function(){
-   console.log("start point...") 
+    getGameData();
 })
