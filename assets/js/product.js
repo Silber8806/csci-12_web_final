2881ec7d50f5979d652b599499a63f7bae11be80
scotty-kotty-myer <ckottmyer@salesforce.com>
2019-04-18 00:07:01 -0400

    creating content sub-parts

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,14 +1,48 @@
 'use strict'
 
+function createGameMetrics(json){
+    
+    var gameMetrics = document.createElement("table");
+    var gameMetricsRow = document.createElement("tr");
+    var gamePlayersHeader = document.createElement("th");
+    var gameTimeHeader = document.createElement("th");
+    var gamePlayers = document.createElement("td");
+    var gameTime = document.createElement("td");
+    
+    $(gameMetrics).addClass("discover-game-stats");
+    $(gameMetricsRow).clone().appendTo(gameMetrics);
+    $(gamePlayersHeader).html("<i class=\"fas fa-users\"></i> Players").appendTo(gameMetrics);
+    $(gameTimeHeader).html("<i class=\"far fa-clock\"></i> Time").appendTo(gameMetrics);
+    $(gameMetricsRow).clone().appendTo(gameMetrics);
+    var gamePlayersText = json['players-min'] + " - " + json['players-max'];
+    var gameTimeText = json['time-min'] + " - " + json['time-max'] + ' min';
+    $(gamePlayers).html(gamePlayersText).appendTo(gameMetrics);
+    $(gameTime).text(gameTimeText).appendTo(gameMetrics); 
+    
+    var gameAgeHeader = document.createElement("th");
+    var gameComplexityHeader = document.createElement("th");
+    var gameAge = document.createElement("td");
+    var gameComplexity = document.createElement("td"); 
+    
+    $(gameMetricsRow).clone().appendTo(gameMetrics);
+    $(gameAgeHeader).html("<i class=\"fas fa-child\"></i> Age").appendTo(gameMetrics);
+    $(gameComplexityHeader).html("<i class=\"fas fa-calculator\"></i> Complexity").appendTo(gameMetrics)
+    $(gameMetricsRow).clone().appendTo(gameMetrics);
+    $(gameAge).text(json['age'] + "+").appendTo(gameMetrics);
+    $(gameComplexity).text(json['complexity'] + " / 5.00").appendTo(gameMetrics); 
+    
+    return gameMetrics
+}
+
 function constructProductPage(json) {
     var current_game = getUrlParameter('game');
+    var current_game_json = json[current_game];
     var contentSection = $("#content");
     
-    # var gameTitleText = json[current_game].title;
-    # var gameTitle = document.createElement("h1");
+    var gameMetrics = createGameMetrics(current_game_json);
     
-    $(gameTitle).text(gameTitleText).appendTo(contentSection);
-    
+    $(gameMetrics).appendTo("#product-metric");
+
     return 0;
 }
 
